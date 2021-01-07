a95533f5cb9133297bd2b32979e061c1262ce749
Klara Birnbaumova <k.birnbaumova@seznam.cz>
2021-01-07 16:52:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from "react";
 import './App.css';
 
 function App() {
+
+  const STARTING_TIME = 15;
+  const textAreaRef = useRef(null);
+
+  const [text, setText] = useState("");
+  const [words, setWords] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(STARTING_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+
+  const changeText = (event) => {
+    const {value} = event.target;
+    setText(value);
+  }
+  
+  const wordsCounter = (text) => {
+    if(isTimeRunning === false) {
+      const wordsArr = (text.trim().split(' '));
+      const wordsArrFilter = wordsArr.filter(word => word !== "").length;
+      setWords(wordsArrFilter);
+    }
+  }
+
+  useEffect(() => {
+    if(isTimeRunning && remainingTime > 0) {
+      setTimeout(() => {
+        setRemainingTime(time => time - 1);
+      }, 1000);
+    } else if(remainingTime === 0) {
+      endGame();
+    }
+  }, [remainingTime, isTimeRunning]);
+
+  const startGame = () => {
+        setIsTimeRunning(true);
+        setRemainingTime(STARTING_TIME);
+        setWords(0);
+        setText("");
+        textAreaRef.current.disabled = false;
+        textAreaRef.current.focus();
+  }
+
+  const endGame = () => {
+    setIsTimeRunning(false)
+    wordsCounter(text);
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Speed typing game</h1>
+      <textarea name="input" 
+                value={text} 
+                onChange={changeText} 
+                disabled={!isTimeRunning} 
+                ref={textAreaRef}
+      />
+      <h4>Remaining: {remainingTime} </h4>
+      <button onClick={startGame} disabled={isTimeRunning}> Start</button>
+      <h1>Your score: {words} words </h1>
     </div>
   );
 }
